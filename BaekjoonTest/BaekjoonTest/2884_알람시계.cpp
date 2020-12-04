d1d41fc14c9a4eeb4290255a678e460e800309ea
ahnsem824 <ahnsem96@gmail.com>
2020-12-04 18:19:06 +0900

    if 문제 마무리 for 문제 생성

--- BaekjoonTest/BaekjoonTest/2884_알람시계.cpp
+++ BaekjoonTest/BaekjoonTest/2884_알람시계.cpp
@@ -4,5 +4,20 @@ using namespace std;
 
 void 알람시계_2884()
 {
+	int h;
+	int m;
 
+	cin >> h >> m;
+	
+	if (m < 45)
+	{
+		if (h == 0)
+		{
+			h = h + 24;
+		}
+		h = h - 1;
+		m= m + 60;
+	}
+	m = m - 45;
+	cout << h << m << endl;
 }
