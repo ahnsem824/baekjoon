f46f945d3e469612e0ca4231fb0ba85c135c9313
ahnsem824 <ahnsem96@gmail.com>
2020-12-08 10:42:22 +0900

    while문제 풀이 1차원배열 최소최대 문제 풀이

--- BaekjoonTest/BaekjoonTest/10871_X보다작은수.cpp
+++ BaekjoonTest/BaekjoonTest/10871_X보다작은수.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void X보다작은수_10871()
 {
+	ios::sync_with_stdio(false), cout.tie(0), cin.tie(0);
 	int N;
 	int X;
 	int a;
--- BaekjoonTest/BaekjoonTest/10951_A+B_4.cpp
+++ BaekjoonTest/BaekjoonTest/10951_A+B_4.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 void AB_5_10952()
 {
-
 	ios::sync_with_stdio(false), cout.tie(0), cin.tie(0); // 동기화를 끊는다
-	while
-
-
+	int a;
+	int b;
+	while (cin >> a >> b)
+	{
+		cout << a + b << '\n';
+	}
 }
