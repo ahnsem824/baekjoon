089d467260de89f6ca3cb9a32c055299f5c3f8a7
ahnsem824 <ahnsem96@gmail.com>
2021-02-09 23:25:38 +0900

    문제풀이 분수찾기

---
+++ BaekjoonTest/BaekjoonTest/1193_분수찾기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void 분수찾기_1193()
+{
+	ios::sync_with_stdio(false), cout.tie(0), cin.tie(0);
+	
+
+	int a;
+
+	cin >> a;
+
+	int i;
+
+	for (i = 1; a > 0; i++)
+	{
+		a = a - i;
+	}
+	if (i % 2 == 1)
+	{
+		cout << i + a - 1 << "/" << 1 - a << endl;
+	}
+	else if (i % 2 == 0)
+	{
+		cout << 1 - a << "/" << i + a - 1 << endl;
+	}
+
+}
