1ac246f7379c797302d644556ea53daa6b4cfa35
ahnsem824 <ahnsem96@gmail.com>
2021-02-12 23:31:31 +0900

    수학 2 문제 추가

---
+++ BaekjoonTest/BaekjoonTest/10250_ACM호텔.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void ACMÈ£ÅÚ_10250()
+{
+	
+
+
+
+}
---
+++ BaekjoonTest/BaekjoonTest/2275_부녀회장이될테야.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void 부녀회장이될테야_2275()
+{
+
+
+
+
+}
