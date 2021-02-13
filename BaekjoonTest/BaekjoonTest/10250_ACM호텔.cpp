a976c7844d3df1723617d350a20846b12aeeb98e
ahnsem824 <ahnsem96@gmail.com>
2021-02-13 21:38:07 +0900

    10250_ ACM호텔 문제 풀이 endl을 안붙여줘서 테스트 케이스 계속 실패함..

--- BaekjoonTest/BaekjoonTest/10250_ACM호텔.cpp
+++ BaekjoonTest/BaekjoonTest/10250_ACM호텔.cpp
@@ -6,7 +6,26 @@ using namespace std;
 void ACMÈ£ÅÚ_10250()
 {
 	
+	
 
+	int count;
+	cin >> count;
+
+	for (int i = 0; i < count; i++)
+	{
+		int w;
+		int h;
+		int n;
+		cin >> h >> w >>n;
+		if (n%h > 0)
+		{
+			cout << n % h * 100 + n / h + 1 << endl;
+		}
+		else
+		{
+			cout << h * 100 + n / h << endl;
+		}
+	}
 
 
 }
