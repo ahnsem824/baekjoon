9f01a06bda8e1310d189625ec06d11f181fc586c
ahnsem824 <ahnsem96@gmail.com>
2021-02-16 21:24:36 +0900

    부녀회장이 될테야 2275 문제풀이 큰수 A+B생성 및 문제 푸는중

---
+++ BaekjoonTest/BaekjoonTest/10757_큰수A+B.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void Å«¼öAB_10757()
+{
+	ios::sync_with_stdio(false), cout.tie(0), cin.tie(0);
+	string a;
+	string b; 
+
+	string max;
+	string min;
+
+	cin >> a >> b;
+	if (a.length() >= b.length())
+	{
+		max = a;
+		min = b;
+	}
+	else
+	{
+		min = a;
+		max = b;
+	}
+	
+	
+	for (int i = 0; i < min.length(); i++)
+	{
+	
+	}
+
+
+}
--- BaekjoonTest/BaekjoonTest/2275_부녀회장이될테야.cpp
+++ BaekjoonTest/BaekjoonTest/2275_부녀회장이될테야.cpp
@@ -5,15 +5,51 @@ using namespace std;
 
 void 부녀회장이될테야_2275()
 {
-	/*int a[15][15];
+	int a[15][15];
 
 	for (int i = 0; i < 15; i++)
 	{
-		for (int j = 0; j < 15; j++)
+		if (i == 0)
 		{
-
-			a[i][j] = 0;
+			for (int j = 1; j < 15; j++) 
+			{
+				a[i][j] = j;
+			}
 		}
-	}*/
+		else
+		{
+			for (int j = 1; j < 15; j++)
+			{
+				if (j == 1)
+				{
+					a[i][j] = 1;
+
+				}
+				else
+				{
+					int t = 0;
+					for (int k = 1; k <= j; k++)
+					{
+						t += a[i - 1][k];
+						
+					}
+					a[i][j] = t;
+				}
+			}
+		}
+	}
+
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		int x;
+		int y; 
+
+		cin >> x >> y;
+
+		cout << a[x][y] << endl;
+	}
 
 }
